8e09e27c5dd495ce0cf82ac6c19c6b77003ec6c8
muniir114 <36700187+muniir114@users.noreply.github.com>
2024-08-14 13:05:47 +0300

    Add files via upload

---
+++ five.py
@@ -0,0 +1,34 @@
+import random
+tiedosto = open("nimet.txt", "r")
+ 
+rivit = tiedosto.readlines()
+
+
+valinta = input("(s)atunnainen rivi, (i)käkysely, (p)oiminta vai (n)imikysel ? ")
+if valinta == "s":
+    print("Tulostetaan satunnaine ")    
+    satrivi = random.choice(rivit)
+    print(satrivi)
+elif valinta == "i":  
+    for rivi in rivit:
+       rivinOsat = rivi.split()
+       rivinVuosi = int(rivinOsat[1])
+       if rivinVuosi >= 2006:
+            print(rivi)
+elif valinta == "p":  
+    numero = int(input("anna numero"))
+    k = 0
+    for rivi in rivit:
+       k += 1
+       if k % numero == 0:
+           print(rivi)
+
+elif valinta == "n":
+    nimi = input("Änna nimi")
+
+    for rivi in rivit:
+        if nimi in rivi:
+            print(rivi)
+ 
+ 
+ 
---
+++ six.py
@@ -0,0 +1,42 @@
+import random
+tiedosto = open("nimet.txt", "r")
+ 
+rivit = tiedosto.readlines()
+
+
+valinta = input("(s)atunnainen rivi, (i)käkysely, (p)oiminta, (n)imikysely vai (k)irjainkysely ? ")
+if valinta == "s":
+    print("Tulostetaan satunnaine ")    
+    satrivi = random.choice(rivit)
+    print(satrivi)
+elif valinta == "i":  
+    for rivi in rivit:
+       rivinOsat = rivi.split()
+       rivinVuosi = int(rivinOsat[1])
+       if rivinVuosi >= 2006:
+            print(rivi)
+elif valinta == "p":  
+    numero = int(input("anna numero"))
+    k = 0
+    for rivi in rivit:
+       k += 1
+       if k % numero == 0:
+           print(rivi)
+
+elif valinta == "n":
+    nimi = input("Änna nimi")
+
+    for rivi in rivit:
+        if nimi in rivi:
+            print(rivi)
+
+
+elif valinta == "k":
+    later = input("Änna irjainkysely")
+
+    for rivi in rivit:
+        if later in rivi:
+            print(rivi)
+ 
+ 
+ 
